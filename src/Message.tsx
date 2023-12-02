8327cff89507cae1ef504475b5a803f9b2c9c5ac
indraworks <indra.works2020@gmail.com>
2023-12-02 09:08:16 +0700

    bab1 - persiapan react-vite

---
+++ src/Message.tsx
@@ -0,0 +1,20 @@
+
+
+const Message = () => {
+   
+  const name ="Indra"
+  if(name) return (
+    <h1>Hello {name}</h1>
+  )   
+  return ( <h1>Hello World!</h1>)}
+
+
+export default Message
+/*
+dgn jsx akan sangat mudah buat kita mmbuat component 
+dan kalau kita buat  javascript variable atau logic maka kita harus buat
+dgn didalam {} contoh diatas kita buat variable nah utk buat smua const  variable awal 
+harus diatas return ,krn didalam return adalah jsx ( html yg dtulis dalam JvSCRIPT)
+
+
+*/
