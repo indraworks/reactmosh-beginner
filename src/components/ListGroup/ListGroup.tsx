50752912c90ac3cc5893ce55d3b40b3129e92f6f
indraworks <indra.works2020@gmail.com>
2023-12-11 13:55:14 +0700

    3.4 CSS in JS <Styled Component>

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,27 @@
 import { useState } from "react";
-import styles from "./ListGroup.module.css";
+//import styles from "./ListGroup.module.css";
+import styled from "styled-components";
+
+//nama ul
+const List = styled.ul`
+  list-style: none;
+  padding: 10px;
+`;
+
+//interface utk props listItem kita buat
+interface PropsListItem {
+  active: boolean;
+}
+
+//nama li
+//kalau mau ngolah bahwa props tsb active true maka warna backgroud berubah
+// nah kita msukin props dgn tanda ${} sbgai ganti variable
+//disini kita mau ngati backround jadi orange kalau si active /yg lagi dklik listitenya
+const ListItem = styled.li<PropsListItem>`
+  
+  padding: 5px 0;
+  background:${(props)=>props.active?'orange':''}
+`;
 
 interface Props {
   items: string[];
@@ -7,21 +29,29 @@ interface Props {
   onSelectedItem: (item: string) => void;
 }
 function ListGroup({ items, heading, onSelectedItem }: Props) {
+  //nah jadi gini utk selectedIndex ini kan suoaya kita tahu
+  // yg dclassname ada namanya active nah kita === selectedIndex apakah sama
+  //nah nti kan itu haru masuk props sptrti ini karrn  melalui styled dianggap component
+  //contoh maka ada props yg masuk distyled component: <ListItem active={} />
+  //nah kita mesti buat prope shape atau type prop indentity lewat interface jadi kita buat intefacenya
+  //nah kita pasang sgai type idenittynya nnti di componet yg akan dimasuki si props!
+  //const ListItem = styled,li<PropsListItem>``;
+  //nah tadi si active itu kan propwety dari si className dari list kalau dia actiove maka akan trsorot
+  //misalkan warna biru etc nah disini kita akan buat logic pada propsnya
+
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h2>{heading}</h2>
       {items.length == 0 && <p>Item not Found !!</p>}
-      <ul className={[styles.listGroup, styles.active].join(" ")}>
+      <List>
         {items.map((item, index) => {
           return (
-            <li
-              className={
-                selectedIndex === index
-                  ? "list-group-item active"
-                  : "list-group-item"
-              }
+            <ListItem
+              //classname kita hapus nnti lewat props utk milih nah props harus dibuatkan
+              //interfacenya intinya itu saja ,brikut props active yg udah di asukin sblum dibuat iterface
+              active={index === selectedIndex}
               key={item}
               // onClick={()=>console.log(item,index)}
               //onClick={(event) => console.log(event)}
@@ -31,10 +61,10 @@ function ListGroup({ items, heading, onSelectedItem }: Props) {
               }} //ini yg akan sebabkan nilai selected index berubah
             >
               {item}
-            </li>
+            </ListItem>
           );
         })}
-      </ul>
+      </List>
     </>
   );
 }
